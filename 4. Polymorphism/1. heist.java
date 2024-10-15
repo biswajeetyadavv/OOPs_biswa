9dc7fd79022c591081caabc4c249ef3f600bdbc6
Biswajeet yadav <biswajeetyadavofficial@gmail.com>
2024-10-15 22:36:22 +0530

    adding new file

---
+++ 4. Polymorphism/1. heist.java
@@ -0,0 +1,54 @@
+package Polymorphism;
+
+public class heist {
+//    as we will study Polymorphism
+//    lets get to known what is it?
+//    Poly means many and morphsm means forms.
+
+//    it is a concept of OOPs.
+//    which states that a single method in terms of java can have
+//    many actions.
+
+//    it means same method can do many actions.
+//    i hope you know what is override.
+//    override means when we crete an extended class (to understand extended class go thorough
+
+
+//    and we declare an existing method (means method already present in parent class)
+//    then after declaring we assign some to actions in it. which may be different from
+//    parent class's method.
+
+//    now, when we create an object of extended class and we call the
+//    method(which is common in parent and extended class) of extended class
+//    it will perform the action which is
+//    present in extended class because the method in extended class has been override.
+
+
+//    one more concept known as MEHTOD OVERLOADING
+//    it means we can have many similar name methods but there signature must be different.
+//    signature means the paremeter, and it no. of paremeter and its types
+
+//    so, that if we want to execure an action but the mehtod which execute it has a name is common.
+//    then how will the complier will detech which action has to perform.
+
+//    action will be detect by the signature the method has.
+//    the thing is all the mehtods are have same name but different signatures.
+//    in terms of signature we are gonna get our work done.
+
+//    lets see how?
+
+    String name;
+    int age;
+
+    void kill(){
+        System.out.println(name + "kill the person");
+    }
+
+//    now, as we have crated a class which will act as parent class.
+//    next step is to ceate an extended class
+//    extended class is vampire
+//    next file.
+
+//    NOTE: files are in sequence make sure to go with sequnce in oreder to underttant the concpept
+
+}
---
+++ 4. Polymorphism/3. vampire.java
@@ -0,0 +1,15 @@
+package Polymorphism;
+
+public class vampire extends heist{
+
+//    even if i do not decalre any method and feild it will work.
+//    if i create an object of vampire in main.java file and ask it to make an action like heist it will work.
+//    as it is an extended/child class of heist.
+//    it has all the fetures of  heist in it.
+
+//    now we will learn how overriding works
+
+
+
+
+}
